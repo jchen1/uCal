f4abcb5b7a2031cbf533637b6c4d715ad7426928
Jeff Chen <jeff@blendlabs.com>
2016-06-25 16:28:07 -0700

    Initial Commit

---
+++ μCal/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  μCal
+//
+//  Created by Jeff Chen on 6/25/16.
+//  Copyright © 2016 Jeff Chen. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ μCal/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  μCal
+//
+//  Created by Jeff Chen on 6/25/16.
+//  Copyright © 2016 Jeff Chen. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
