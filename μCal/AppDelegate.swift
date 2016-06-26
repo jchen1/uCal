262f2a62d6a5e00015eab1432757ff788ca688ff
Jeff Chen <jeff@blendlabs.com>
2016-06-25 20:28:31 -0700

    Initial commit

--- μCal/AppDelegate.swift
+++ μCal/AppDelegate.swift
@@ -10,11 +10,68 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-
+    
+    var statusBar = NSStatusBar.systemStatusBar();
+    var statusItem: NSStatusItem?;
+    var menu = NSMenu();
+//    var menuItem = NSMenuItem();
+    let datePicker = NSDatePicker();
+    
+    let dateFormatter = NSDateFormatter();
+    var timer = NSTimer();
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+        statusItem = statusBar.statusItemWithLength(NSVariableStatusItemLength);
+        statusItem?.button?.image = NSImage(named: "switchIcon.png");
+        statusItem?.button?.image?.template = true;
+        statusItem?.button?.imagePosition = NSCellImagePosition.ImageLeft;
+        
+        dateFormatter.dateFormat = "EEE h:mm:ss aa";
+        
+        datePicker.calendar = NSCalendar.autoupdatingCurrentCalendar();
+        datePicker.datePickerMode = NSDatePickerMode.SingleDateMode;
+        datePicker.datePickerStyle = NSDatePickerStyle.ClockAndCalendarDatePickerStyle;
+        datePicker.datePickerElements = NSDatePickerElementFlags.YearMonthDayDatePickerElementFlag;
+        datePicker.bezeled = false;
+        datePicker.dateValue = NSDate();
+        
+        updateTime();
+        
+        statusItem?.menu = menu;
+        
+        let menuItem: NSMenuItem! = menu.addItemWithTitle("item", action: nil, keyEquivalent: "");
+        print(datePicker.frame.size.width, datePicker.frame.size.height);
+        
+        menuItem.view = datePicker;
+        var f = datePicker.frame;
+        f.size.width = 140;
+        f.size.height = 150;
+        datePicker.frame = f;
+        print(datePicker.frame.size.width, datePicker.frame.size.height);
+        
+        let secondItem = NSMenuItem();
+        secondItem.title = "hello";
+        menu.addItem(secondItem);
+        
+        let time = NSDate();
+        
+        let currentSecond = time.timeIntervalSinceReferenceDate;
+        let timeToAdd = ceil(currentSecond) - currentSecond + 0.1;
+        
+        timer = NSTimer.init(fireDate: NSDate.init(timeInterval: timeToAdd, sinceDate: time), interval: 1.0, target: self, selector: #selector(AppDelegate.updateTime), userInfo: nil, repeats: true);
+        
+        NSRunLoop.currentRunLoop().addTimer(timer, forMode: NSDefaultRunLoopMode);
+        
+        //todo: listen to NSSystemClockDidChangeNotification
+    }
+    
+    func updateTime() {
+        let currentTime = NSDate();
+        let timeString = dateFormatter.stringFromDate(currentTime);
+        
+        statusItem?.button?.title = timeString;
+        statusItem?.button?.alternateTitle = timeString;
+        datePicker.dateValue = currentTime;
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
---
+++ μCal/CalendarMenuItemView.swift
@@ -0,0 +1,20 @@
+//
+//  CalendarMenuItemView.swift
+//  μCal
+//
+//  Created by Jeff Chen on 6/25/16.
+//  Copyright © 2016 Jeff Chen. All rights reserved.
+//
+
+import Foundation
+import Cocoa
+
+class CalendarMenuItemView: NSView {
+    required init(frameRect: NSRect) {
+        super.init(frame: frameRect);
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder);
+    }
+}
